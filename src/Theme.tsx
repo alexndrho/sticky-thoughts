bd320f10d7af24f19edba1c6d9856de50e5a91c2
Alexander Ho <ho.alexander.g@gmail.com>
2023-08-13 18:47:07 +0800

    feat: add theme storage and preference

--- src/Theme.tsx
+++ src/Theme.tsx
@@ -3,17 +3,36 @@ import {
   ColorSchemeProvider,
   MantineProvider,
 } from '@mantine/core';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 interface ThemeProps {
   children: React.ReactNode;
 }
 
 const Theme = ({ children }: ThemeProps) => {
-  const [colorScheme, setColorScheme] = useState<ColorScheme>('dark');
+  const [colorScheme, setColorScheme] = useState<ColorScheme>('light');
+
+  useEffect(() => {
+    const storedColorScheme = localStorage.getItem('colorScheme');
+
+    if (storedColorScheme) {
+      setColorScheme(storedColorScheme as ColorScheme);
+    } else {
+      const systemColorScheme = window.matchMedia(
+        '(prefers-color-scheme: dark)'
+      ).matches
+        ? 'dark'
+        : 'light';
+
+      setColorScheme(systemColorScheme);
+    }
+  }, []);
 
   const toggleColorScheme = (value?: ColorScheme) => {
-    setColorScheme(value || colorScheme === 'dark' ? 'light' : 'dark');
+    const newColorScheme = value || colorScheme === 'dark' ? 'light' : 'dark';
+
+    localStorage.setItem('colorScheme', newColorScheme);
+    setColorScheme(newColorScheme);
   };
 
   return (
