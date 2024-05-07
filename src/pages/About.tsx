734411ddfe2421c7688b1bab88a2e82247101e5f
Alexander Ho <ho.alexander.g@gmail.com>
2024-05-07 10:36:57 +0800

    fix: remove unnecessary divs

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -14,52 +14,50 @@ const About = ({ title }: AboutProps) => {
 
   return (
     <AppContainer>
-      <Box py="7.5rem">
-        <Flex align="center" gap="md">
-          <Box>
-            <Title>
-              Sticky
-              <Text span c="blue.6" inherit>
-                Thoughts
-              </Text>
-              <Text span display="block" fz="xl" fs="italic">
-                A place to share your thoughts.
-              </Text>
-            </Title>
-            <Text mt="lg" fz="lg">
-              StickyThoughts offers a platform where you can freely express
-              yourself and share your thoughts and experiences with others. You
-              have the option to remain anonymous or not. You can post anything
-              that you want including your thoughts, experiences, and even your
-              secrets. You can also read other people's notes. The notes can be
-              customized by using different colors, and you can search for other
-              people's notes by their names. There is no limit to the number of
-              notes you can add.
+      <Flex py="7.5rem" align="center" gap="md">
+        <Box>
+          <Title>
+            Sticky
+            <Text span c="blue.6" inherit>
+              Thoughts
             </Text>
-            <Text mt="lg" fz="lg"></Text>
-            <Text mt="lg" fz="lg">
-              StickyThoughts is an open source project available on{' '}
-              <Anchor
-                href="https://github.com/alexndrho/sticky-thoughts"
-                target="_blank"
-                inherit
-              >
-                Github
-              </Anchor>
-              , created by{' '}
-              <Anchor href="https://alexndrho.dev" target="_blank" inherit>
-                Alexander Gabriel Ho
-              </Anchor>
-              . This online freedom wall serves as a platform to provide an
-              emotional outlet for its users.
+            <Text span display="block" fz="xl" fs="italic">
+              A place to share your thoughts.
             </Text>
-          </Box>
+          </Title>
+          <Text mt="lg" fz="lg">
+            StickyThoughts offers a platform where you can freely express
+            yourself and share your thoughts and experiences with others. You
+            have the option to remain anonymous or not. You can post anything
+            that you want including your thoughts, experiences, and even your
+            secrets. You can also read other people's notes. The notes can be
+            customized by using different colors, and you can search for other
+            people's notes by their names. There is no limit to the number of
+            notes you can add.
+          </Text>
+          <Text mt="lg" fz="lg"></Text>
+          <Text mt="lg" fz="lg">
+            StickyThoughts is an open source project available on{' '}
+            <Anchor
+              href="https://github.com/alexndrho/sticky-thoughts"
+              target="_blank"
+              inherit
+            >
+              Github
+            </Anchor>
+            , created by{' '}
+            <Anchor href="https://alexndrho.dev" target="_blank" inherit>
+              Alexander Gabriel Ho
+            </Anchor>
+            . This online freedom wall serves as a platform to provide an
+            emotional outlet for its users.
+          </Text>
+        </Box>
 
-          <Box display={{ base: 'none', xs: 'block' }}>
-            <Image src={noteImg} w={250} />
-          </Box>
-        </Flex>
-      </Box>
+        <Box display={{ base: 'none', xs: 'block' }}>
+          <Image src={noteImg} w={250} />
+        </Box>
+      </Flex>
     </AppContainer>
   );
 };
--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Flex, Text, Title } from '@mantine/core';
+import { Button, Flex, Text, Title } from '@mantine/core';
 import { useEffect } from 'react';
 import AppContainer from '../components/AppContainer';
 import { IconMail } from '@tabler/icons-react';
@@ -14,30 +14,28 @@ const Contact = ({ title }: ContactProps) => {
 
   return (
     <AppContainer>
-      <Box py="7.5rem">
-        <Flex direction="column" justify="center" align="center">
-          <Title ta="center" c="blue">
-            Get in Touch
-          </Title>
+      <Flex py="7.5rem" direction="column" justify="center" align="center">
+        <Title ta="center" c="blue">
+          Get in Touch
+        </Title>
 
-          <Text span fz="xl" fs="italic" ta="center" display="block">
-            We would love to hear from you.
-          </Text>
+        <Text span fz="xl" fs="italic" ta="center" display="block">
+          We would love to hear from you.
+        </Text>
 
-          <Text fz="xl" ta="center">
-            If you have any questions, please feel free to reach out.
-          </Text>
+        <Text fz="xl" ta="center">
+          If you have any questions, please feel free to reach out.
+        </Text>
 
-          <Flex my="md" align="center" gap="xs" fz="xl">
-            <IconMail size="1.25em" />
-            ho.alexander.g@gmail.com
-          </Flex>
-
-          <Button component="a" href="mailto:ho.alexander.g@gmail.com">
-            Send a Mail
-          </Button>
+        <Flex my="md" align="center" gap="xs" fz="xl">
+          <IconMail size="1.25em" />
+          ho.alexander.g@gmail.com
         </Flex>
-      </Box>
+
+        <Button component="a" href="mailto:ho.alexander.g@gmail.com">
+          Send a Mail
+        </Button>
+      </Flex>
     </AppContainer>
   );
 };
