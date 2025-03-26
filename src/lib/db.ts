675d98170d2c9017ec7fe2f5e9180c1e5e97cec0
Alexander Gabriel Ho <80305180+alexndrho@users.noreply.github.com>
2025-03-26 20:15:41 +0800

    fix(prisma): use edge version of prisma client

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client/edge";
 import { thoughtInput } from "./validations/thought";
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
