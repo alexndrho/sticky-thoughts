e751d9c29b6327f6792dd6914a059c4f3dc1d153
Alexander Gabriel Ho <80305180+alexndrho@users.noreply.github.com>
2025-03-27 22:28:52 +0800

    fix(prisma): use standard prisma to use docker

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client/edge";
+import { PrismaClient } from "@prisma/client";
 import { createThoughtInput } from "./validations/thought";
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
