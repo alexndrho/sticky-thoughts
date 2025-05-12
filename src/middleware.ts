e4f9804e28793f8856448c697c7f3e8db1fd989d
Alexander Gabriel Ho <80305180+alexndrho@users.noreply.github.com>
2025-05-12 23:21:11 +0800

    fix(middleware): add matching any pattern using

--- src/middleware.ts
+++ src/middleware.ts
@@ -12,5 +12,5 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/forum/submit", "/settings"],
+  matcher: ["/forum/submit(.*)", "/settings(.*)"],
 };
