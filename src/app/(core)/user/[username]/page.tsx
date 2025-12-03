1f8de2008e8face6b5541a35eb83d5abca91f5f6
Alexander Gabriel Ho <ho.alexander.g@gmail.com>
2025-12-03 14:04:58 +0800

    fix(profile): add not found handling

--- src/app/(core)/user/[username]/page.tsx
+++ src/app/(core)/user/[username]/page.tsx
@@ -6,15 +6,20 @@ import { useParams } from "next/navigation";
 import { userUsernameOptions } from "@/app/(core)/user/options";
 import Content from "./Content";
 import ContentSkeleton from "@/components/ContentSkeleton";
+import { NotFoundContent } from "@/app/not-found";
 
 export default function UserPage() {
   const params = useParams<{ username: string }>();
 
-  const userQuery = useQuery(userUsernameOptions(params.username));
-
-  return (
-    <>
-      {userQuery.data ? <Content user={userQuery.data} /> : <ContentSkeleton />}
-    </>
+  const { data: user, isLoading: isUserLoading } = useQuery(
+    userUsernameOptions(params.username),
   );
+
+  if (user) {
+    return <Content user={user} />;
+  } else if (isUserLoading) {
+    return <ContentSkeleton />;
+  }
+
+  return <NotFoundContent />;
 }
