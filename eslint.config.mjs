653c5d8934db34152de52f0d192e614785c5e8a6
Alexander Gabriel Ho <ho.alexander.g@gmail.com>
2025-12-11 16:07:26 +0800

    fix(eslint): update configuration and dependencies

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,20 +1,22 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
+import nextTypescript from "eslint-config-next/typescript";
 import eslintConfigPrettier from "eslint-config-prettier/flat";
 import pluginQuery from "@tanstack/eslint-plugin-query";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...nextCoreWebVitals,
+  ...nextTypescript,
   ...pluginQuery.configs["flat/recommended"],
   eslintConfigPrettier,
+  {
+    ignores: [
+      "node_modules/**",
+      ".next/**",
+      "out/**",
+      "build/**",
+      "next-env.d.ts",
+    ],
+  },
 ];
 
 export default eslintConfig;
