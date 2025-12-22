3680f6159a7a50ac430fe69bfb191e592a837310
Alexander Gabriel Ho <ho.alexander.g@gmail.com>
2025-12-22 12:14:36 +0800

    fix(thread): simplify layout and improve styling for thread items

--- src/app/(core)/threads/ThreadItem.tsx
+++ src/app/(core)/threads/ThreadItem.tsx
@@ -1,4 +1,13 @@
-import { Avatar, Flex, Group, Paper, Text, Title } from "@mantine/core";
+import {
+  Anchor,
+  Avatar,
+  Box,
+  Flex,
+  Group,
+  Paper,
+  Text,
+  Title,
+} from "@mantine/core";
 import Link from "next/link";
 import { formatDistance } from "date-fns";
 
@@ -19,63 +28,83 @@ export default function ThreadItem({ post, onLike }: ThreadItemProps) {
     <Paper
       component="article"
       shadow="xs"
+      pos="relative"
       withBorder
       className={classes.thread}
     >
-      <Flex
-        component={Link}
-        href={`/threads/${post.id}`}
-        direction="column"
-        className={classes.thread__content}
-      >
-        <header>
-          <Flex align="center" gap="xs">
-            <Avatar src={post.author.image} size="xs" />
+      <Box p="md">
+        <Link
+          href={`/threads/${post.id}`}
+          className={classes["thread__main-link"]}
+          aria-label={`View thread titled ${post.title}`}
+        />
 
-            <Text size="sm">
-              {post.author.name || post.author.username} <span>•</span>{" "}
-              <span>
+        <Flex direction="column">
+          <header>
+            <Flex align="center" gap="xs">
+              <Avatar
+                component={Link}
+                pos="relative"
+                size="xs"
+                src={post.author.image}
+                href={`/user/${post.author.username}`}
+                aria-label={`View profile of ${post.author.username}`}
+              />
+
+              <Text size="sm">
+                <Anchor
+                  component={Link}
+                  pos="relative"
+                  href={`/user/${post.author.username}`}
+                  className={classes.thread__link}
+                >
+                  {post.author.name || post.author.username}
+                </Anchor>{" "}
+                •{" "}
                 {formatDistance(new Date(post.createdAt), new Date(), {
                   addSuffix: true,
                 })}
-              </span>
-            </Text>
-          </Flex>
-          <Title order={2} size="h3" lineClamp={2}>
-            {post.title}
-          </Title>
-        </header>
+              </Text>
+            </Flex>
+            <Title order={2} size="h3" lineClamp={2}>
+              {post.title}
+            </Title>
+          </header>
 
-        <Text lineClamp={5}>{stripHtmlTags(post.body)}</Text>
-      </Flex>
+          <Text lineClamp={5}>{stripHtmlTags(post.body)}</Text>
+        </Flex>
 
-      <Group className={classes["thread__action-container"]}>
-        <LikeButton
-          liked={post.likes.liked}
-          count={post.likes.count}
-          size="compact-sm"
-          onLike={(e) => {
-            e.preventDefault();
+        <Group mt="md">
+          <LikeButton
+            pos="relative"
+            liked={post.likes.liked}
+            count={post.likes.count}
+            size="compact-sm"
+            onLike={(e) => {
+              e.preventDefault();
 
-            onLike?.({
-              id: post.id,
-              like: !post.likes.liked,
-            });
-          }}
-        />
+              onLike?.({
+                id: post.id,
+                like: !post.likes.liked,
+              });
+            }}
+          />
 
-        <CommentButton
-          component={Link}
-          href={`/threads/${post.id}`}
-          count={post.comments.count}
-          size="compact-sm"
-        />
+          <CommentButton
+            pos="relative"
+            component={Link}
+            href={`/threads/${post.id}`}
+            count={post.comments.count}
+            size="compact-sm"
+          />
 
-        <ShareButton
-          size="compact-sm"
-          link={`${process.env.NEXT_PUBLIC_BASE_URL}/threads/${post.id}`}
-        />
-      </Group>
+          <ShareButton
+            pos="relative"
+            size="compact-sm"
+            link={`${process.env.NEXT_PUBLIC_BASE_URL}/threads/${post.id}`}
+          />
+        </Group>
+      </Box>
     </Paper>
   );
 }
